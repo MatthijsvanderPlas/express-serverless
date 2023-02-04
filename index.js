0e404e779c53753f8c694a2d58bec70c3909de28
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-02-04 10:09:35 +0100

    Initial commit

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ app.get('/', (req, res) => {
   res.status(200).send('Hello Serverless World!')
 })
 
+app.get('/about', (req, res) => {
+  const url = req.url
+  const user = req.ip
+  console.log(url, ip)
+  res.status(200).send('About page')
+})
+
 module.exports.handler = serverless(app)
