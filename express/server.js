1a5066eda1dd941bd4fe3b01047891acfdc9626e
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-02-04 10:42:59 +0100

    First run of the server

--- express/server.js
+++ express/server.js
@@ -1,23 +1,25 @@
-const express = require('express')
-const serverless = require('serverless-http')
+"use strict"
+const express = require("express")
+const serverless = require("serverless-http")
 const app = express()
+const bodyParser = require("body-parser")
 const router = express.Router()
-
-app.use('/.netlify/functions/server', router)
-app.use('/', router)
-
-router.get('/', (req, res) => {
-  res.writeHead(200, { "content-type": "text/html" })
-  res.write('<h1>Up and running!</h1>')
+app.use(bodyParser.json())
+app.use("/.netlify/functions/server", router) // path must route to lambda
+app.use("/", router)
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" })
+  res.write("<h1>Up and running</h1>")
   res.end()
 })
-
-app.get('/about', (req, res) => {
-  const url = req.url
-  const user = req.ip
-  console.log(url, ip)
-  res.status(200).send('About page')
+router.post("/doSomething", async (req, res) => {
+  try {
+    // maybe do some database interaction or third-party API call here!
+    res.status(200).send({ data: "success" })
+  } catch (err) {
+    console.log(err)
+    res.status(400).send({ error: "bad request" })
+  }
 })
-
 module.exports = app
 module.exports.handler = serverless(app)
